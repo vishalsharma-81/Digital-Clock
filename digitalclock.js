33100ae1a7187931fbfbeef8cf8eb160f2c3b4e8
vishal sharma <vishalsharma2682@gmail.com>
2025-07-16 18:28:38 +0530

    Initial commit of Digital Clock  App Project

---
+++ digitalclock.js
@@ -0,0 +1,18 @@
+// Digital clock program
+
+function updateClock(){
+
+
+    const now = new Date();
+    let hours = now.getHours();
+    const meridien = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    hours = hours.toString().padStart(2,  0);
+    const minutes = now.getMinutes().toString().padStart(2, 0);
+    const seconds = now.getSeconds().toString().padStart(2, 0);
+    const timeString = `${hours}:${minutes}:${seconds} ${meridien}`;
+    document.getElementById("clock").textContent= timeString;
+
+}
+ updateClock();
+ setInterval(updateClock, 1000);
